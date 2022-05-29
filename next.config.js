5f1f3f4d3f4f8be5e5e5cee2285abb1c3109d1a2
Bùi Ngọc Yên <buingocyen055@gmail.com>
2022-05-29 10:59:09 +0700

    feature/update json file

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/docs/:path*',
+        destination: '/:path*', // The :path parameter is used here so will not be automatically passed in the query
+      },
+    ]
+  },
+}
